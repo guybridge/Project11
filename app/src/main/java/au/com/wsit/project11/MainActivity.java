8a858290df26ac2b89a23c641ebad60576a0bd5c
Guy Bridge <guy@wsit.com.au>
2017-01-02 15:45:33 +0800

    Added logo and new launcher

--- app/src/main/java/au/com/wsit/project11/MainActivity.java
+++ app/src/main/java/au/com/wsit/project11/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setLogo(R.mipmap.ic_action_logo);
+        getSupportActionBar().setTitle("");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
